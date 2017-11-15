c5dd3563c9bee1889f2818d7e8c51055201ff5c0
yunzou <yunzou@bainainfo.com>
2017-11-15 18:29:33 +0800

    init

---
+++ app/src/main/java/com/example/yunzou/connect/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yunzou.connect
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
